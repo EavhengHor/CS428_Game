fa5c3cbcd9a8be4d4be262b56aa1ef40131cad0e
Eavheng Hor <ehor@paragoniu.edu.kh>
2026-04-01 00:09:26 +0700

    completed the main manu

---
+++ User_Interface_Scene/Main_Menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	print("Start Pressed")
+	get_tree().change_scene_to_file("res://All_Level_Scene/Level_1/level_1.tscn")
+
+
+func _on_continue_button_pressed() -> void:
+	print("Continue Pressed")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Exit Pressed")
+	get_tree().quit()
